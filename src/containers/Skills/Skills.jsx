be83b5a204e5e0fc82b4f7ed72498802a90fc85e
Brilliant-Developer <email2obaidul@gmail.com>
2022-03-23 23:27:07 +0600

    2nd

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
-import './Skills.scss'
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+import ReactTooltip from 'react-tooltip';
+import './Skills.scss';
 
 const Skills = () => {
+  const [experience, setExperience] = useState([])
+  const [skills, setSkills] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "experience"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query)
+    .then((data) => {
+      setExperience(data);
+    })
+    client.fetch(skillsQuery)
+    .then((data) => {
+      setSkills(data);
+    })
+  },[])
   return (
-    <div>Skills</div>
+    <>
+    <h2 className='head-text'> <span>Skills</span> & Experience</h2>
+
+    <div className='app__skills-container'>
+      <motion.div className='app__skills-list'>
+        {skills.map((skill) => (
+          <motion.div
+          whileInView={{ opacity:[0, 1]}}
+          transition={{ duration: 0.5 }}
+          className="app__skills-item app__flex"
+          key={skill.name}
+          >
+          <div className='app__flex' style={{backgroundColor: skill.bgColor }}>
+          <img src={urlFor(skill.icon)} alt={skill.name} />
+          </div>
+
+          <p className='p-text'>{skill.name}</p>
+          </motion.div>
+        ))}
+      </motion.div>
+
+      <motion.div className='app__skills-exp'>
+      {experience.map((work => (
+        <>
+          <motion.div
+          whileInView={{ opacity:[0, 1]}}
+          transition={{ duration: 0.5 }}
+          className="app__skills-exp-work"
+          data-tip
+          data-for={work.name}
+          key={work.name}
+          >
+          <h4 className='bold-text'>{work.name}</h4>
+          <p className='p-text'>{work.company}</p>
+          </motion.div>
+
+          <ReactTooltip
+          id={work.name}
+          effect="solid"
+          arrowColor='#fff'
+          className='skills-tooltip'
+          >
+          {work.desc}
+          </ReactTooltip>
+        </>
+      )))}
+      </motion.div>
+    </div>
+    </>
   )
 }
 
