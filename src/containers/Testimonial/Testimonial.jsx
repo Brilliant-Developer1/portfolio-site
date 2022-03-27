861160a696b90ba384526dfb3e15adf3693ca76b
Brilliant-Developer <email2obaidul@gmail.com>
2022-03-27 23:32:08 +0600

    testimonials added

--- src/containers/Testimonial/Testimonial.jsx
+++ src/containers/Testimonial/Testimonial.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import './Testimonial.scss'
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+import './Testimonial.scss';
 const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
-}
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default Testimonial
+  useEffect(() => {
+    // const query = '*[_type == "experiences"]';
+    const testimonialsQuery = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(testimonialsQuery).then(data => {
+      setTestimonials(data);
+    });
+    client.fetch(brandsQuery).then(data => {
+      setBrands(data);
+    });
+  }, []);
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex"></div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg'
+);
